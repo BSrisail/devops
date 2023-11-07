c4a0b2899d11879062561ab5e47895725b9f2b9a
bparamasivasrisai <srisail.09.09@gmail.com>
2023-11-07 03:26:01 -0800

    first change

--- Frontend.sh
+++ Frontend.sh
@@ -3,9 +3,7 @@ cp expense.conf /etc/nginx/default.d/expense.conf
 
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-
-cd /usr/share/ngnix/html
-unzip /temp/frontend.zip
-
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
 systemctl enable nginx
 systemctl restart nginx
