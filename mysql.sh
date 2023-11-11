f57698ab281768d6ecb07f75db3fa58855ba8a74
bparamasivasrisai <srisail.09.09@gmail.com>
2023-11-11 02:08:25 -0800

    first change

--- mysql.sh
+++ mysql.sh
@@ -1,13 +1,13 @@
-$log= &>>/tmp/expense.log
+log= /tmp/expense.log
 
 echo -e "\e[32m installing nginx \e[0m"
-dnf module disable mysql -y $log
+dnf module disable mysql -y &>>log
 
 echo -e "\e[33m copy expense \e[0m"
-cp mysql.repo /etc/yum.repos.d/mysql.repo $log
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>log
 
 echo -e "\e[35m enable nginx \e[0m"
-dnf install mysql-community-server -y $log
+dnf install mysql-community-server -y &>>log
 
 echo -e "\e[34m enable nginx \e[0m"
 systemctl enable mysqld
