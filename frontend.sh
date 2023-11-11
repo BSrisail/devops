e22fe68498d6d420fba38cfaf155d8d0285d10bc
bparamasivasrisai <srisail.09.09@gmail.com>
2023-11-11 01:07:58 -0800

    first change

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,12 @@
+echo -e "\e[32m installing nginx \e[0m"
 dnf install nginx -y
 
+echo -e "\e[33m copy expense \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf
+
+echo -e "\e[33m enable nginx \e[0m"
 systemctl enable nginx
+echo -e "\e[34m start nginx \e[0m"
 systemctl start nginx
 
 rm -rf /usr/share/ngnix/html/*
