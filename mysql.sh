9223f62313107d087fe49877b31198110bb596b9
bparamasivasrisai <srisail.09.09@gmail.com>
2023-11-11 02:02:15 -0800

    first change

--- mysql.sh
+++ mysql.sh
@@ -1,9 +1,16 @@
-dnf module disable mysql -y
+echo -e "\e[32m installing nginx \e[0m"
+dnf module disable mysql -y &>>/tmp/expense.log
 
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e "\e[33m copy expense \e[0m"
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/expense.log
 
-dnf install mysql-community-server -y
+echo -e "\e[35m enable nginx \e[0m"
+dnf install mysql-community-server -y &>>/tmp/expense.log
+
+echo -e "\e[34m enable nginx \e[0m"
 systemctl enable mysqld
+
+echo -e "\e[36m enable nginx \e[0m"
 systemctl start mysqld
 
 mysql_secure_installation --set-root-pass ExpenseApp@1
