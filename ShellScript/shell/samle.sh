d40fe3c13678806f7ceb19538565d121b377b36a
bparamasivasrisai <srisail.09.09@gmail.com>
2023-11-22 23:18:34 -0800

    first change

--- ShellScript/shell/samle.sh
+++ ShellScript/shell/samle.sh
@@ -1,6 +1,6 @@
 line=$(head -n 11 txt | tail -n 1)
-$?
-if [ $? -eq 0 ]; then
+exit_code= $?
+if [ $exit_code -eq 0 ]; then
     echo "The 11th line is: $line"
 else
     echo "The file does not have 11 lines."
