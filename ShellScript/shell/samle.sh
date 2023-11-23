6287888a106fbd411de66527901b899e823c75e3
bparamasivasrisai <srisail.09.09@gmail.com>
2023-11-22 23:26:30 -0800

    first change

--- ShellScript/shell/samle.sh
+++ ShellScript/shell/samle.sh
@@ -1,8 +1,14 @@
 line=$(head -n 11 txt | tail -n 1)
+
+# Store the exit code of the last command (in this case, the command substitution)
 exit_code=$?
-if [ $exit_code -eq 0 ]; then
+
+# Print the exit code
+echo "Exit code: $exit_code"
+
+# Print the 11th line (if found)
+if [ -n "$line" ]; then
     echo "The 11th line is: $line"
 else
     echo "The file does not have 11 lines."
 fi
-
