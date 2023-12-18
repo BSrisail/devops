c924cd7b79086103bab3b674e68cc07606177b67
bparamasivasrisai <srisail.09.09@gmail.com>
2023-12-17 23:35:55 -0800

    first change

--- terraform/Sample.tf
+++ terraform/Sample.tf
@@ -1,13 +1,10 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-
 resource "aws_instance" "c2_instance" {
   count         = length(var.names)
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
-  subnet_id        = var.existing_subnet_id
   security_groups  = [var.existing_sg_id]
   tags = {
     Name = var.names[count.index]
