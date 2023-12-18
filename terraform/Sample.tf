bd5d93d88e35a0ac08cceab88ac3a32d85e078bf
bparamasivasrisai <srisail.09.09@gmail.com>
2023-12-17 23:53:39 -0800

    first change

--- terraform/Sample.tf
+++ terraform/Sample.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "c2_instance" {
   count         = length(var.names)
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
-  security_groups  = [var.existing_sg_id]
+  vpc_security_group_ids  = [var.existing_sg_id]
   tags = {
     Name = var.names[count.index]
   }
